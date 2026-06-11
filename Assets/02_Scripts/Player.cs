d806471a2c925b15fd6a363f035fc15bf2d29b06
hyeontae12 <thkim980416@gmail.com>
2026-06-11 18:27:40 +0900

    feat:왼쪽이동시 플레이어 flip

--- Assets/02_Scripts/Player.cs
+++ Assets/02_Scripts/Player.cs
@@ -7,11 +7,13 @@ public class Player : MonoBehaviour
     private Rigidbody2D _rb2D;
     private Vector2 _moveInput;
     private float _playerSpeed;
+    private SpriteRenderer _spriteRenderer;
 
     private void Awake()
     {
         _rb2D = GetComponent<Rigidbody2D>();
         _playerSpeed = playerInfo.basePlayerSpeed;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnMove(InputValue value)
@@ -19,6 +21,14 @@ public class Player : MonoBehaviour
         _moveInput = value.Get<Vector2>();
     }
 
+    private void Update()
+    {
+        if (_moveInput.x != 0)
+        {
+            _spriteRenderer.flipX = _moveInput.x < 0;
+        }
+    }
+    
     private void FixedUpdate()
     {
         _rb2D.linearVelocity = new Vector2(_moveInput.x * _playerSpeed, _moveInput.y * _playerSpeed);
